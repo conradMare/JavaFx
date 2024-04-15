4bb997bc481765d584092e9d23df8a8abc079f5d
conradMare <conradmare4@gmail.com>
2024-04-15 05:24:10 +0200

    2024/04/15(am)
    ToDoList - S25.435_Base Interface

--- ToDoList/src/main/java/com/example/todolist/HelloController.java
+++ ToDoList/src/main/java/com/example/todolist/HelloController.java
@@ -1,6 +1,10 @@
 package com.example.todolist;
 
 import com.example.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -10,6 +14,12 @@ import java.util.List;
 public class HelloController {
 
     private List<TodoItem> todoItems;
+
+    @FXML
+    private ListView<TodoItem> todoListView;
+
+    @FXML
+    private TextArea itemDetailsTextArea;
 
     public void initialize() {
         TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
@@ -29,5 +39,19 @@ public class HelloController {
         todoItems.add(item3);
         todoItems.add(item4);
         todoItems.add(item5);
+
+        todoListView.getItems().setAll(todoItems);
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    @FXML
+    public void handleClickListView() {
+        TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+//        System.out.println("The selected item is " + item);
+        StringBuilder sb = new StringBuilder(item.getDetails());
+        sb.append("\n\n\n\n");
+        sb.append("Due: ");
+        sb.append(item.getDeadline().toString());
+        itemDetailsTextArea.setText(sb.toString());
     }
 }
