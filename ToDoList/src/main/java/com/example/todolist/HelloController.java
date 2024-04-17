4554bec964664aade4f54bb92b069f99e3454e9e
conradMare <conradmare4@gmail.com>
2024-04-17 09:20:15 +0200

    2024/04/17(am)
    ToDoList - S25.439_Load and Save ToDo Items from/to Disk

--- ToDoList/src/main/java/com/example/todolist/HelloController.java
+++ ToDoList/src/main/java/com/example/todolist/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.todolist;
 
+import com.example.todolist.datamodel.TodoData;
 import com.example.todolist.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -9,10 +10,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 public class HelloController {
@@ -29,23 +27,25 @@ public class HelloController {
     private Label deadlineLabel;
 
     public void initialize() {
-        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
-                LocalDate.of(2016, Month.APRIL, 25));
-        TodoItem item2 = new TodoItem("Doctors appointment", "See Dr.Smith at 123 Main Street",
-                LocalDate.of(2016, Month.MAY, 23));
-        TodoItem item3 = new TodoItem("Finish Design Proposal", "I promised Mike I'd finish website mockups by Friday 22nd April",
-                LocalDate.of(2016, Month.APRIL, 22));
-        TodoItem item4 = new TodoItem("Pick up Doug at the train station", "Doug is arriving March 23 on the 5:00 train",
-                LocalDate.of(2016, Month.MARCH, 23));
-        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
-                LocalDate.of(2016, Month.APRIL, 20));
-
-        todoItems = new ArrayList<TodoItem>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
+//        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 30th birthday card for John",
+//                LocalDate.of(2016, Month.APRIL, 25));
+//        TodoItem item2 = new TodoItem("Doctors appointment", "See Dr.Smith at 123 Main Street",
+//                LocalDate.of(2016, Month.MAY, 23));
+//        TodoItem item3 = new TodoItem("Finish Design Proposal", "I promised Mike I'd finish website mockups by Friday 22nd April",
+//                LocalDate.of(2016, Month.APRIL, 22));
+//        TodoItem item4 = new TodoItem("Pick up Doug at the train station", "Doug is arriving March 23 on the 5:00 train",
+//                LocalDate.of(2016, Month.MARCH, 23));
+//        TodoItem item5 = new TodoItem("Pick up dry cleaning", "The clothes should be ready by Wednesday",
+//                LocalDate.of(2016, Month.APRIL, 20));
+//
+//        todoItems = new ArrayList<TodoItem>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
 
         todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
             @Override
@@ -61,7 +61,8 @@ public class HelloController {
             }
         });
 
-        todoListView.getItems().setAll(todoItems);
+//        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
         todoListView.getSelectionModel().selectFirst();
