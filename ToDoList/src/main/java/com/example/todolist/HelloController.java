797f5d696b1e840256789003b9d065fc7530cfd1
conradMare <conradmare4@gmail.com>
2024-04-24 10:18:02 +0200

    2024/04/24(am)
    ToDoList - S25.446_KeyEvents

--- ToDoList/src/main/java/com/example/todolist/HelloController.java
+++ ToDoList/src/main/java/com/example/todolist/HelloController.java
@@ -9,6 +9,8 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.util.Callback;
@@ -131,6 +133,17 @@ public class HelloController {
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
             todoListView.getSelectionModel().select(newItem);
+        }
+    }
+
+    // EventHandler for key press:
+    @FXML
+    public void handleKeyPressed(KeyEvent keyEvent) {
+        TodoItem selectedItem = todoListView.getSelectionModel().getSelectedItem();
+        if (selectedItem != null) {
+            if (keyEvent.getCode().equals(KeyCode.DELETE)) {
+                deleteItem(selectedItem);
+            }
         }
     }
 
