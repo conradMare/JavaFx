76733de6a3061fec4b097b810d43a3ac465da71f
conradMare <conradmare4@gmail.com>
2024-04-25 08:01:20 +0200

    2024/04/25(am)
    JavaFxApplication - S25.450_Transforming Nodes and Choosers

--- JavaFXApplication/src/main/java/com/example/javafxapplication/HelloController.java
+++ JavaFXApplication/src/main/java/com/example/javafxapplication/HelloController.java
@@ -4,11 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+   @FXML
+    private Label label;
+
+   @FXML
+    public void initialize() {
+       label.setScaleX(2.0);
+       label.setScaleY(2.0);
+   }
 }
