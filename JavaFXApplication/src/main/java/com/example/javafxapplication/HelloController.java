735c590d4ddb2379fb32c05a02dac28ecb5943ea
conradMare <conradmare4@gmail.com>
2024-04-25 08:40:22 +0200

    2024/04/25(am)
    JavaFXApplication - S25.450_Transforming Nodes and Choosers

--- JavaFXApplication/src/main/java/com/example/javafxapplication/HelloController.java
+++ JavaFXApplication/src/main/java/com/example/javafxapplication/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.javafxapplication;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
 
    @FXML
     private Label label;
 
+   @FXML
+   private Button button4;
+
+   @FXML
+   private GridPane gridPane;
+
     public void initialize() {
+        button4.setEffect(new DropShadow());
    }
 
    @FXML
@@ -21,5 +35,18 @@ public class HelloController {
     public void handleMouseExit() {
        label.setScaleX(1.0);
        label.setScaleY(1.0);
+   }
+
+   @FXML
+    public void handleClick() {
+//       FileChooser chooser = new FileChooser();
+//       chooser.showOpenDialog(gridPane.getScene().getWindow());
+       DirectoryChooser chooser = new DirectoryChooser();
+       File file = chooser.showDialog(gridPane.getScene().getWindow());
+       if (file != null) {
+           System.out.println(file.getPath());
+       } else {
+           System.out.println("Chooser was cancelled");
+       }
    }
 }
