8dc88a8d63b49f318c0a30ec63807ca0e372cbd8
conradMare <conradmare4@gmail.com>
2024-04-26 05:00:33 +0200

    2024/04/26(am)
    SceneBuilder - S25.455_Overview of SceneBuilder

--- SceneBuilder/src/main/java/module-info.java
+++ SceneBuilder/src/main/java/module-info.java
@@ -8,7 +8,6 @@ module com.example.scenebuilder {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     opens com.example.scenebuilder to javafx.fxml;
